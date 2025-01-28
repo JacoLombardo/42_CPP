0986f91165d8e173707d984b435381c625be71d6
Jacopo Lombardo <jalombar@c3a3c6.42berlin.de>
2025-01-28 13:42:52 +0100

    ex05-bis

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,13 +6,23 @@
 /*   By: jalombar <jalombar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 10:19:38 by jalombar          #+#    #+#             */
-/*   Updated: 2024/12/18 11:15:05 by jalombar         ###   ########.fr       */
+/*   Updated: 2025/01/28 13:27:09 by jalombar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main(void)
+int main(int argc, char **argv)
 {
+    Harl main;
+    std::string comment;
+    
+    if (argc == 2)
+    {
+        comment = argv[1];
+        for(std::size_t i = 0; i < comment.length(); i++)
+            comment[i] = std::tolower(static_cast<unsigned char>(comment[i]));
+        main.complain(comment);
+    }
     return (0);
 }
