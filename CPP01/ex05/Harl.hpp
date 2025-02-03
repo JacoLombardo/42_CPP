d4f8509168c9879adbef9037af9f406f527f5c74
Jacopo Lombardo <jalombar@c3a3c7.42berlin.de>
2025-02-03 16:03:55 +0100

    done

--- CPP01/ex05/Harl.hpp
+++ CPP01/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: jalombar <jalombar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 10:20:41 by jalombar          #+#    #+#             */
-/*   Updated: 2025/01/28 13:42:29 by jalombar         ###   ########.fr       */
+/*   Updated: 2025/02/03 15:51:44 by jalombar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,38 +21,15 @@
 class Harl
 {
     private:
-        void debug(void) 
-        {
-            std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
-        }
-        void info(void) 
-        {
-            std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
-        }
-        void warning(void) 
-        {
-            std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
-        }
-        void error(void) 
-        {
-            std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
-        }
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
         std::map<std::string, void (Harl::*)()> functionMap;
 
     public:
-        Harl()
-        {
-            functionMap["debug"] = &Harl::debug;
-            functionMap["info"] = &Harl::info;
-            functionMap["warning"] = &Harl::warning;
-            functionMap["error"] = &Harl::error;
-        }
-        void complain(std::string level)
-        {
-            std::map<std::string, void (Harl::*)()>::iterator it = functionMap.find(level);
-            if (it != functionMap.end())
-                (this->*(it->second))();
-        }
+        Harl();
+        void complain(std::string level);
 };
 
 #endif
--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jalombar <jalombar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 10:19:38 by jalombar          #+#    #+#             */
-/*   Updated: 2025/01/28 13:27:09 by jalombar         ###   ########.fr       */
+/*   Updated: 2025/02/03 16:02:51 by jalombar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ int main(int argc, char **argv)
         comment = argv[1];
         for(std::size_t i = 0; i < comment.length(); i++)
             comment[i] = std::tolower(static_cast<unsigned char>(comment[i]));
-        main.complain(comment);
+        if (!comment.compare("debug") || !comment.compare("info") || !comment.compare("warning") || !comment.compare("error"))
+            main.complain(comment);
+        else
+            std::cout << "Harl does not complain about that" << std::endl;
     }
     return (0);
 }
