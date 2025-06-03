43fc42ca1c5655b1d3a058372daf7577c5671d4a
JacoLombardo <jacopo.lombardo@outlook.com>
2025-06-03 13:52:30 +0200

    fix

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -12,18 +12,30 @@
 
 #include "whatever.hpp"
 
-int main(void) {
-    int a = 2;
-    int b = 3;
-    ::swap( a, b );
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-    std::string c = "test1";
-    std::string d = "test2";
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+int main(void)
+{
+	{
+		std::cout << "TESTING WITH INTEGERS:" << std::endl;
+		int a = 2;
+		int b = 3;
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		::swap( &a, &b );
+		std::cout << "swapping..." << std::endl;
+			std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	}
+
+	{
+		std::cout << std::endl << "TESTING WITH STRINGS:" << std::endl;
+		std::string c = "test1";
+		std::string d = "test2";
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		::swap( &c, &d );
+		std::cout << "swapping..." << std::endl;
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
     return (0);
 }
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,22 +6,26 @@
 /*   By: jalombar <jalombar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 11:30:44 by jalombar          #+#    #+#             */
-/*   Updated: 2025/05/19 12:12:34 by jalombar         ###   ########.fr       */
+/*   Updated: 2025/06/03 13:52:08 by jalombar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int main(void) {
+int main(void)
+{
+	{
+		std::cout << "TESTING WITH INT ARRAY:" << std::endl;
+		int array2[] = {1, 2, 3, 4};
+		iter(array2, 4, testFunction<int>);
+		std::cout << std::endl;
+	}
 
-	std::cout << "Testing with string array:" << std::endl;
-	std::string array1[] = {"ciao", "come", "stai", "?"};
-	iter(array1, 4, testFunction<std::string>);
-	std::cout << std::endl;
-
-	std::cout << "Testing with int array:" << std::endl;
-	int array2[] = {1, 2, 3, 4};
-	iter(array2, 4, testFunction<int>);
-	std::cout << std::endl;
+	{
+		std::cout << "TESTING WITH STRING ARRAY:" << std::endl;
+		std::string array1[] = {"ciao", "come", "stai", "?"};
+		iter(array1, 4, testFunction<std::string>);
+		std::cout << std::endl;
+	}
 	return (0);
 }
